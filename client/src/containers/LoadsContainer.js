2e022e2d4fdbe46e629f8e05bbbff6fa2066b33d
Chris Norwood <cnorwood92@gmail.com>
2019-10-08 16:24:24 -0700

    Remove previous api call to loads, until auth complete

--- client/src/containers/LoadsContainer.js
+++ client/src/containers/LoadsContainer.js
@@ -4,9 +4,10 @@ import { getLoads } from '../services/tmsApi'
 export const LoadsContainer = () => {
   const [loads, setLoads] = useState([])
 
-  useEffect(() => {
-    getLoads().then(data => setLoads(data))
-  }, [])
+  // Commented out until Auth complete
+  // useEffect(() => {
+  //   getLoads().then(data => setLoads(data))
+  // }, [])
 
   return (
     <pre>{JSON.stringify(loads, null, 2)}</pre>
--- client/src/services/tmsApi.js
+++ client/src/services/tmsApi.js
@@ -4,8 +4,10 @@ export const baseUrl = '/api/v1/'
 export const loadsUrl = `${baseUrl}loads.json`
 
 export const getLoads = async () => {
-  const result = await axios.get(loadsUrl)
-    .then(({ data }) => data)
+  // Commented Out Until Auth Complete
+  // Consider switch from axios to Ky
+  // const result = await axios.get(loadsUrl)
+  //   .then(({ data }) => data)
 
-  return result
+  // return result
 }
