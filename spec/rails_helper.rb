2cc884e33642d53cf893ce45cebe58b22210215f
Chris Norwood <cnorwood92@gmail.com>
2019-10-07 17:54:58 -0700

    Basic Auth class and specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,7 +58,9 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   # include my JSON->ruby hash helper
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  # include JWT and Auth Helpers
+  config.include ControllerSpecHelper
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
