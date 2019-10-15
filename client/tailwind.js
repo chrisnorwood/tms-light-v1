84a03872ac8d8b9b658b3b824f5c1bd4f2db023f
Chris Norwood <cnorwood92@gmail.com>
2019-10-15 16:13:12 -0700

    Add support for even/odd psuedo classes and backgrounColor

--- client/tailwind.js
+++ client/tailwind.js
@@ -38,6 +38,7 @@ module.exports = {
   variants: {
     opacity: ['responsive', 'hover', 'focus', 'disabled'],
     cursor: ['responsive', 'hover', 'focus', 'disabled'],
+    backgroundColor: ['even', 'odd'],
   },
   plugins: []
 }
