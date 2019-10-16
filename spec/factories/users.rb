1e7bebcc9599f8badfbfc6bdd8a650da96663cf4
Chris Norwood <cnorwood92@gmail.com>
2019-10-15 20:10:40 -0700

    Fix tests to include unique emails and password confirmation inclusion

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :user do
+    sequence :email do |n|
+      "email#{n}#{rand(200).to_s}#{rand(200).to_s}@factory.com"
+    end
+
     name { Faker::Name.name }
-    email { 'foo@bar.com' } 
     password { 'foobar' }
+    password_confirmation { 'foobar' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,5 @@ RSpec.describe User, type: :model do
   # validations
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:password_confirmation) }
 end
