b66b6502975e548772d225fc87c01c259ff72f2b
Chris Norwood <cnorwood92@gmail.com>
2019-10-26 11:38:38 -0700

    include defaultExractor in purgeCSS config to avoid stripping tailwind classes with symbols

--- client/purgecss.config.js
+++ client/purgecss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['build/static/index.html', 'build/static/js/*.js'],
   css: ['build/static/css/*.css'],
-  whitelistPatterns: [/Toastify(.*)/]
+  whitelistPatterns: [/Toastify(.*)/],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 }
